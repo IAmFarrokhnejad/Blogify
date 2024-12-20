c900cc17f2491a3f163fd383e03656d48bfc1cdf
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-12-20 18:17:51 +0300

    Dashboard (WIP)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "./components/dashboard/ThemeProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.className} ${geistMono.className} antialiased`}
-      >
-        {children}
+        className={`${geistSans.className} ${geistMono.className} antialiased`}>
+           <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
